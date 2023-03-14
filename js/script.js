45c0917816d85327f6c387a66c8424e3c37c6482
saleemshahzad08 <pmaf16e018@gmail.com>
2023-03-14 15:56:14 +0500

    UPDATED

---
+++ js/script.js
@@ -0,0 +1,3 @@
+function searchBox() {
+	document.getElementById("search").innerHTML = "<div class='input-group input-group-sm'> <span class='input-group-text' id='inputGroup-sizing-sm'>Search</span> <input type='text' class='form-control' aria-label='Sizing example input' aria-describedby='inputGroup-sizing-sm'></div>";
+}
